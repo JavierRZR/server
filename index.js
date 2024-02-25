ed091586eb916db8455159039d9dc1bddb587d97
Javi <ruizromero98@gmail.com>
2024-02-25 18:22:51 +0100

    Fix room management.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const cors = require('cors');
-const { v4: uuidV4 } = require('uuid');
+// const { v4: uuidV4 } = require('uuid');
 
-const appUrl = process.env.APP_URL || 'https://learnguage.vercel.app/';
+const appUrl = process.env.APP_URL || 'http://localhost:3000';
 
 
 
@@ -19,7 +19,7 @@ app.use(cors());
 
 
 app.get('/:room', (req, res) => {
-    res.redirect(`${appUrl}/${uuidV4()}`);
+    res.redirect(`${appUrl}/${req.params.room}`);
 })
 
 
@@ -29,9 +29,12 @@ io.on('connection', (socket) => {
         socket.to(roomId).emit('user-connected', userId);
     })
 
-    socket.on('disconnect', (socket) => {
-        console.log("disconnect")
-        // socket.to(roomId).emit('user-disconnected', userId)
+    socket.on('disconnect', () => {
+        // console.log("disconnect")
+    })
+
+    socket.on('disconnectMe', ({ roomId, userId }) => {
+        socket.to(roomId).emit('user-disconnected', userId)
     })
 
     socket.on('emit-message', (msg, roomId) => {
